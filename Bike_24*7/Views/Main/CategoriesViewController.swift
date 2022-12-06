171dc5438e1a6904b30ad0988e75d4809322c3eb
Pratik <chougulepratik07@gmail.com>
2022-12-05 21:33:21 -0800

    d

--- Bike_24*7/Views/Main/CategoriesViewController.swift
+++ Bike_24*7/Views/Main/CategoriesViewController.swift
@@ -44,6 +44,7 @@ class CategoriesViewController: UIViewController, UICollectionViewDelegate, UICo
     }
     
     
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: 190, height: 250)
     }
